53327912f490a840584d56bb69371fcadc387fba
AbdullahAlHadabi <Abdullah.Juma280@hotmail.com>
2026-03-09 09:26:18 +0400

    "Number Ladder: The Climbing Count" "Step Up: The Staircase Challenge"

---
+++ AdvancedLoop.java
@@ -0,0 +1,28 @@
+public class AdvancedLoop {
+    public static void main(String[] args){
+
+
+
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int s = 1; s <= 5-i; s++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <=2*i-1; k++) {
+
+                if(k==1 || k==2*i-1 || i==5 ){
+                    System.out.print("*");
+
+                }else {
+                    System.out.print(" ");
+                }
+
+
+            }
+            System.out.println();
+        }
+
+    }
+}
